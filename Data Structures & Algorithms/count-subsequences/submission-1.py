16d4f391c4a77e1a453750f5bfd09e14b3ded36c
anamay1234 <112140395+anamay1234@users.noreply.github.com>
2026-05-24 17:55:34 -0700

    Add: count-subsequences - submission-1

---
+++ Data Structures & Algorithms/count-subsequences/submission-1.py
@@ -0,0 +1,32 @@
+class Solution:
+    # memo
+    def func(self, i, j, s, t, dp):
+        if j == len(t):
+            return 1
+
+        if i == len(s):
+            return 0
+
+        if dp[i][j] != -1:
+            return dp[i][j]
+        
+        if s[i] == t[j]:
+            pick = self.func(i+1, j+1, s, t, dp)
+            noPick = self.func(i+1, j, s, t, dp)
+
+            dp[i][j] = pick + noPick
+            return dp[i][j]
+        
+        if s[i] != t[j]:
+            noPick = self.func(i+1, j, s, t, dp)
+
+            dp[i][j] = noPick
+            return dp[i][j]
+        
+
+    def numDistinct(self, s: str, t: str) -> int:
+        dp = [[-1] * len(t) for i in range(len(s))]
+        return self.func(0, 0, s, t, dp)
+
+        # i: 0 -> len(s)
+        # j: 0 -> len(t)
