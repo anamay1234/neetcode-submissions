057a84fa29ceefdf836562790863e6264fdc323a
anamay1234 <112140395+anamay1234@users.noreply.github.com>
2026-06-03 14:00:57 -0700

    Add: max-area-of-island - submission-7

---
+++ Data Structures & Algorithms/max-area-of-island/submission-7.py
@@ -0,0 +1,27 @@
+class Solution:
+    # my code
+    def func(self, i, j, grid, visited):
+        if i < 0 or i >= len(grid) or j < 0 or j >= len(grid[0]) or grid[i][j] == 0 or (i, j) in visited:
+            return 0
+
+        visited.add((i, j))
+
+        left = self.func(i, j - 1, grid, visited)
+        right = self.func(i, j + 1, grid, visited)
+        up = self.func(i - 1, j, grid, visited)
+        down = self.func(i + 1, j, grid, visited)
+
+        return 1 + left + right + up + down
+
+    def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
+
+        visited = set()
+        maxArea = 0
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
+                if grid[i][j] == 1:
+                    maxArea = max(maxArea, self.func(i, j, grid, visited))
+
+
+        return maxArea
+        
