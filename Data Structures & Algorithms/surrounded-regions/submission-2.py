14d20fec8ebc665564e4a0d265d41f257a8637b6
anamay1234 <112140395+anamay1234@users.noreply.github.com>
2026-06-03 13:13:13 -0700

    Add: surrounded-regions - submission-2

---
+++ Data Structures & Algorithms/surrounded-regions/submission-2.py
@@ -0,0 +1,61 @@
+class Solution:
+    # my code
+    def func(self, i, j, board): # used to traverse thru graph
+        if i < 0 or i >= len(board) or j < 0 or j >= len(board[0]) or board[i][j] == 'X' or board[i][j] == 'placeholderforO':
+            return
+
+        board[i][j] = 'placeholderforO'
+
+        self.func(i - 1, j, board)
+        self.func(i + 1, j, board)
+        self.func(i, j - 1, board)
+        self.func(i, j + 1, board)
+
+
+    def solve(self, board: List[List[str]]) -> None:
+
+        print(board)
+
+        for i in range(len(board)):     
+            if board[i][0] == 'O':
+                print('i: ', i, 'j: ', 0, board[i][0])
+        
+                self.func(i, 0, board)
+
+            if board[i][len(board[0]) - 1] == 'O':
+                self.func(i, len(board[0]) - 1, board)
+        
+        for j in range(len(board[0])):
+            if board[0][j] == 'O':
+                self.func(0, j, board)
+
+            if board[len(board) - 1][j] == 'O':
+                self.func(len(board) - 1, j, board)
+        
+        print(board)
+
+        for i in range(len(board)):
+            for j in range(len(board[0])):
+                # filling in surrounded Regions
+                
+                if board[i][j] == 'O':
+                    print('i ', i, 'j', j, board[i][j])
+                    board[i][j] = 'X'
+
+                    print('i ', i, 'j', j, board[i][j])
+
+                # reverting Back
+                if board[i][j] == 'placeholderforO':
+                    board[i][j] = 'O'
+
+
+
+
+# replace any 'O's starting at border with a placeholder 
+# and any 'O's connected to the border 'O's.
+# 
+# Then the only 'O's left in theboard must be surrounded.
+# Those O's should be converted to 'X'
+
+# Finally revert the 'O's at the border and their connected 'O's back to 'O'
+        
