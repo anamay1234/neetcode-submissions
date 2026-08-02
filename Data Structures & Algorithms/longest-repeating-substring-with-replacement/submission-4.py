e888dd2e60b22612f0f0a08eb968d2b6677f0a71
anamay1234 <112140395+anamay1234@users.noreply.github.com>
2026-08-02 14:10:42 -0700

    Add: longest-repeating-substring-with-replacement - submission-4

---
+++ Data Structures & Algorithms/longest-repeating-substring-with-replacement/submission-4.py
@@ -0,0 +1,18 @@
+class Solution:
+    def characterReplacement(self, s: str, k: int) -> int:
+
+        L = 0
+        longest = 0
+        hashmap = {}
+
+        for R in range(len(s)):
+            hashmap[s[R]] = hashmap.get(s[R], 0) + 1
+            
+            while (R - L + 1) - max(hashmap.values()) > k:
+                hashmap[s[L]] -= 1
+                L += 1
+            
+            longest = max(longest, (R - L + 1))
+
+        return longest
+        
