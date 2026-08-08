8bc069fedc278d43568ccbcc9cbf59e07a002c97
anamay1234 <112140395+anamay1234@users.noreply.github.com>
2026-08-07 21:22:41 -0700

    Add: serialize-and-deserialize-binary-tree - submission-1

---
+++ Data Structures & Algorithms/serialize-and-deserialize-binary-tree/submission-1.py
@@ -0,0 +1,66 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Codec:
+    
+    # Encodes a tree to a single string.
+    def serialize(self, root: Optional[TreeNode]) -> str:
+        if not root:
+            return ""
+
+        res = []
+        q = deque([root])
+
+        while q:
+            curNode = q.popleft()
+
+            if curNode is None:
+                res.append("#")
+            else:
+                res.append(str(curNode.val))
+                q.append(curNode.left)
+                q.append(curNode.right)
+
+        return ",".join(res)
+
+        
+    # Decodes your encoded data to tree.
+    def deserialize(self, data: str) -> Optional[TreeNode]:
+        if not data:
+            return None
+
+        # Split string by commas into a list: [1, 2, 3, #, #, 4, 5, #, #, #, #]
+        values = iter(data.split(","))
+
+        # Initialize root node
+        first_val = next(values)
+        root = TreeNode(int(first_val))
+
+        q = deque([root])
+
+        while q:
+            node = q.popleft()
+
+            # Process left child
+            left_val = next(values)
+            if left_val == "#":
+                node.left = None
+            else:
+                left_node = TreeNode(int(left_val))
+                node.left = left_node
+                q.append(left_node)
+
+            # Process right child
+            right_val = next(values)
+            if right_val == "#":
+                node.right = None
+            else:
+                right_node = TreeNode(int(right_val))
+                node.right = right_node
+                q.append(right_node)
+
+        return root
